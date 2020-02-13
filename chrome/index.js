32c708a06d48ac8b656aa084b7be3036dc057643
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2020-02-12 21:57:03 -0800

    Update index.js

--- chrome/index.js
+++ chrome/index.js
@@ -4,20 +4,16 @@ require('chromedriver');
 const {Builder, By, Key, until, Capabilities, logging} = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 
-logging.installConsoleHandler();
-//logging.getLogger('webdriver.http').setLevel(logging.Level.ALL);
-//logging.getLogger('webdriver').setLevel(logging.Level.ALL);
-
 (async function example() {
-  var chromeCapabilities = Capabilities.chrome();
-  var chromeOptions = {
-    'args': ['--no-sandbox', '--headless', '--disable-gpu', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-  };
-  chromeCapabilities.set('chromeOptions', chromeOptions);
+  const options = new chrome.Options();
+  options.addArguments("headless");
+  options.addArguments("no-sandbox");
+  options.addArguments("disable-gpu");
+  options.addArguments("disable-setuid-sandbox");
+  options.addArguments("disable-dev-shm-usage");
   const driver = await new Builder()
   .forBrowser('chrome')
-  .withCapabilities(chromeCapabilities)
-  .setChromeOptions(new chrome.Options().headless())
+  .setChromeOptions(options)
   .build();
 
   try {
